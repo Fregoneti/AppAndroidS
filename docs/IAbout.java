d06496fe7782acabd877ac945a8f3df5f2118f9f
Carlos <arresu.ca@gmail.com>
2021-03-03 11:17:49 +0100

    Help casi terminado

---
+++ docs/IAbout.java
@@ -0,0 +1,19 @@
+package interfaces;
+
+public interface IAbout {
+
+    public interface View {
+
+        void goBack();
+
+    }
+
+    public interface Presenter {
+
+
+
+        void onBack();
+
+
+    }
+}
